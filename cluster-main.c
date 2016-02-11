99a5f138cda6a9b4a35585b9bc7c74c29e631a37
Alan Tavares <alan1tavares@gmail.com>
2016-02-10 23:40:50 -0300

    Adicionado uma funcao que ploata esferas no plano

--- cluster-main.c
+++ cluster-main.c
@@ -11,11 +11,11 @@
 GLfloat fAspecto;
 
 // Fim das variaveis globais
-
-void desenha(void);
 void desenhar_eixos(void);
-void altera_tamnaho_janela(GLsizei w, GLsizei h);
+void desenhar_esfera(float x, float y, float z);
+void desenha(void);
 void visualizacao_perspectiva(void);
+void altera_tamnaho_janela(GLsizei w, GLsizei h);
 void inicializa();
 //----------------------------
 // Função principal do sistema
@@ -25,7 +25,7 @@ int main(int argc, char *argv[])
 	//Inicialização de alguns parâmetros
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	//glutInitWindowPosition(x,y);
+	glutInitWindowPosition(5,5);
 	glutInitWindowSize(450,450); //-> tem q alterar isso aq
 	glutCreateWindow("SCluster 3D");
 	//-----------------------------//
@@ -66,8 +66,10 @@ void desenha(void)
 	// Desenha os eixos xyz
 	desenhar_eixos();
 
-	// Desenha uma esfera - solida
-	glutWireSphere(1, 30, 30);
+	// Desenha esferas de cor preta
+	glColor3f(0.0f, 0.0f, 0.0f);
+	desenhar_esfera(2.0f, 0.0f, 0.0f);
+	desenhar_esfera(0.0f, 3.0f, 0.0f);
 
     // Executa os camando OpenGl
 	glFlush();
@@ -76,14 +78,13 @@ void desenha(void)
 
 void altera_tamnaho_janela(GLsizei w, GLsizei h)
 {
-
+	// Para previnir uma divisão por zero
+	//if ( h == 0 ) h = 1;
 	glViewport(0, 0, w, h);
 	fAspecto = (GLfloat)w/(GLfloat)h;
 	visualizacao_perspectiva();
 
-
 }
-
 // fim das fucoes callback;
 
 
@@ -104,7 +105,7 @@ void visualizacao_perspectiva(void)
 	glLoadIdentity();
 
 	// Especifica a posicao do observador e do alvo
-	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+	gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0);
 } // fim da visualizacao de perspectiva
 
 
@@ -135,18 +136,37 @@ void desenhar_eixos(void)
 	glBegin(GL_LINES);
 
 	   // eixo x
-	   glColor3f(0.0f, 0.39f, 0.0f);
+	   glColor3f(1.0f, 0.0f, 0.0f);
 	   glVertex3f(  0.0f, 0.0f, 0.0f);
 	   glVertex3f(720.0f, 0.0f, 0.0f);
 	   // eixo y
-	   glColor3f(0.24f, 0.17f, 0.12f);
+	   glColor3f(0.0f, 1.0f, 0.0f);
 	   glVertex3f(0.0f,   0.0f, 0.0f);
 	   glVertex3f(0.0f, 720.0f, 0.0f);
 	   // eixo z
-	   glColor3f(0.5f, 0.5f, 0.0f);
+	   glColor3f(0.0f, 0.0f, 1.0f);
 	   glVertex3f(0.0f,0.0f, 0.0f);
 	   glVertex3f(0.0f,0.0f, 720.0f);
 
 	glEnd();
 
-} // Fim das funcoes de desenho
+} 
+
+void desenhar_esfera(float x, float y, float z)
+{
+
+	// Guarda a transformacao de matrix corrente na pilha
+	glPushMatrix();
+
+	// Aplica uma translacao sobre a esfera
+	glTranslatef(x, y, z);
+
+	// Desenha uma esfera - solida
+	glutWireSphere(1, 30, 30);
+
+	// Restaura a transformacao de matrix corrente na pilha
+	glPopMatrix();
+
+}
+
+// Fim das funcoes de desenho
