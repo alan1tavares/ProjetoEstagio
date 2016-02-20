19a150328cac8090f2f8791c5ad7a66bf0750a7a
brunex92 <brunex92@gmail.com>
2016-02-20 09:39:24 -0300

    Adicionado iluminação

--- cluster-main.c
+++ cluster-main.c
@@ -51,7 +51,15 @@ void inicializa(void);
 //----------------------------------------------------//
 
 
+const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
+const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
 
+const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
+const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
+const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
+const GLfloat high_shininess[] = { 100.0f };
 //----------------------------
 // Função principal do sistema
 //----------------------------
@@ -60,7 +68,7 @@ int main(int argc, char *argv[])
     //Inicialização de alguns parâmetros
     //-----------------------------//
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
     glutInitWindowPosition(5,5);
     glutInitWindowSize(450,450); //-> tem q alterar isso aq
     glutCreateWindow("SCluster 3D");
@@ -82,6 +90,27 @@ int main(int argc, char *argv[])
 
     inicializa();
     // Inicia o precessamento e guarda interacoes
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LESS);
+
+    glEnable(GL_LIGHT0);
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+
     glutMainLoop();
 
     return 0;
@@ -100,7 +129,7 @@ void desenha(void)
 {
 
     // Limpa a janela de visualizacao
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     // Desenha os eixos xyz
     desenhar_eixos();
@@ -110,6 +139,7 @@ void desenha(void)
     desenhar_esfera(0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f);
 
     drawCylinder(0.0f, 3.0f, 0.0f, 0.5f, 0.5f, 0.5f, 0.0f, 0.0f, 0.0f);
+    drawCylinder(2.0f, 0.0f, 0.0f, 0.5f, 0.5f, 0.5f, 1.0f, 0.08f, 0.58f);
     /*glBegin(GL_LINES);
     glVertex3f(0.5f, 0.5f, 0.5f);
     glVertex3f(2.0f, 0.0f, 0.0f);
@@ -120,6 +150,7 @@ void desenha(void)
 
     // Executa os camando OpenGl
     glFlush();
+    glutSwapBuffers();
 
 
 } // fim desenha
@@ -305,7 +336,8 @@ void inicializa(void)
 //------------------------------------------------//
 void desenhar_eixos(void)
 {
-   glBegin(GL_LINES);
+    glDisable(GL_LIGHTING);
+    glBegin(GL_LINES);
     for (int i = -50; i <= 50; i+=1){
         //plano xz
         glColor3f(1.0f, 0.0f, 0.0f);
@@ -326,9 +358,7 @@ void desenhar_eixos(void)
         glVertex3f(0.0f, (float)i, -50.0f);
         glVertex3f(0.0f, (float)i, 50.0f);
     }
-
-    glEnd();desenhar_esfera(2.0f, 0.0f, 0.0f, 1.0f, 0.08f, 0.58f);
-    desenhar_esfera(0.0f, 3.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+    glEnd();
 
 
 }
@@ -338,7 +368,7 @@ void desenhar_eixos(void)
 //-----------------------------------------------------//
 void desenhar_esfera(float x, float y, float z, float r, float g, float b)
 {
-
+    glEnable(GL_LIGHTING);
     // Guarda a transformacao de matrix corrente na pilha
     glPushMatrix();
 
